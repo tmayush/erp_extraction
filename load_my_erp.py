4c21d69063d6d5053d6558c3bb043e7c2649d094
unkn0wn <unkn0wn@unkn0wn.dev>
2022-08-05 22:48:15 +0530

    Used BeautifulSoup to retrieve attendance details
    BeautifulSoup parses the data from the generated html file and saves it
    into a csv file

--- load_my_erp.py
+++ load_my_erp.py
@@ -1,10 +1,11 @@
-import requests, os, json
+import requests, csv, os, json
+from bs4 import BeautifulSoup
 import util_helper
 
 GLOBAL = {}
 
 
-def get_cred(GLOBAL) -> None:
+def get_cred(GLOBAL: dict, cred_filepath: str) -> None:
     """Retrieves the credentials from the credentials.json file from
     the current directory
 
@@ -12,7 +13,7 @@ def get_cred(GLOBAL) -> None:
         GLOBAL (dict): Stores all the global variables used throughout
         the program
     """
-    with open("credentials.json") as file:
+    with open(cred_filepath) as file:
         json_data = json.loads(file.read())
         GLOBAL["username"] = json_data["username"]
         GLOBAL["password"] = json_data["password"]
@@ -22,7 +23,7 @@ def set_headers(session_obj: requests.Session) -> None:
     """Sets the headers before sending any requests
 
     Args:
-        session_obj (requests.Session): session object for the current 
+        session_obj (requests.Session): session object for the current
         session
     """
     injected_headers = {
@@ -48,7 +49,7 @@ def set_headers(session_obj: requests.Session) -> None:
     session_obj.headers.update(injected_headers)
 
 
-def visit(session_obj: requests.Session, erp_url: str) -> None:
+def visit(session_obj: requests.Session, erp_url: str, homepage_filepath: str) -> None:
     username = GLOBAL["username"]
     password = GLOBAL["password"]
 
@@ -82,19 +83,56 @@ def visit(session_obj: requests.Session, erp_url: str) -> None:
     pwd_visit_res = session_obj.post(erp_url, data)
     print(f"{pwd_visit_res.status_code} - after giving password status code")
 
-    with open("homepage.html", "w+") as f:
+    with open(homepage_filepath, "w+") as f:
         f.write(pwd_visit_res.content.decode("utf8"))
 
     print("Successfully Generated Homepage")
 
 
+def createcsv(csv_filepath: str, homepage_filepath: str) -> None:
+    print(f"Extracting data from {homepage_filepath}")
+
+    soup = None
+    # Stores the main table that wraps the attendance content
+    main_table = None
+
+    with open(homepage_filepath, "r") as file:
+        soup = BeautifulSoup(file, features="html.parser")
+
+    # get all tables and filter with the specific ID to get the attendance table
+    all_tables = soup.find_all("table")
+    for table in all_tables:
+        if table.get("id") == "ctl00_cpStud_grdSubject":
+            main_table = table
+
+    with open(csv_filepath, "w", encoding="UTF8", newline="") as f:
+        writer = csv.writer(f)
+        for tr in main_table.find_all("tr"):
+            row_data = []
+            for th in tr.children:
+                # stripping because some text may have new lines at the end
+                element_text = th.text.strip()
+                row_data.append(element_text)
+
+            # The following locations contain empty strings
+            row_data.pop(0)
+            row_data.pop(-1)
+
+            writer.writerow(row_data)
+
+    print(f"Generated a CSV file - {csv_filepath}")
+
+
 def main():
-    get_cred(GLOBAL)
-    url = "https://erp.cbit.org.in/beeserp/Login.aspx?"
-    "ReturnUrl=%2fbeeserp%2f"
+    csv_file_path = "attendance.csv"
+    homepage_file_path = "homepage.html"
+    get_cred(GLOBAL, "credentials.json")
+    url = "https://erp.cbit.org.in/beeserp/Login.aspx?ReturnUrl=%2fbeeserp%2f"
     with requests.Session() as cur_ses:
         set_headers(cur_ses)
-        visit(cur_ses, url)
+        visit(cur_ses, url, homepage_file_path)
+
+    createcsv(csv_file_path, homepage_file_path)
 
 
 if __name__ == "__main__":
