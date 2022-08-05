dc6c98ce978f8dd43f06b13778898a3d08861cc8
unkn0wn <unkn0wn@unkn0wn.dev>
2022-08-05 17:24:23 +0530

    Code cleanup by adding infered return statements

--- load_my_erp.py
+++ load_my_erp.py
@@ -18,15 +18,14 @@ def get_cred(GLOBAL) -> None:
         GLOBAL["password"] = json_data["password"]
 
 
-def set_headers(session_obj: requests.Session):
+def set_headers(session_obj: requests.Session) -> None:
     """Sets the headers before sending any requests
 
     Args:
-        headers (CaseInsensitiveDict): Stores all the headers
+        session_obj (requests.Session): session object for the current 
+        session
     """
     injected_headers = {
-        # "Cookie": "_pbjs_userid_consent_data",
-        "User-Agent": "hello",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
@@ -49,43 +48,42 @@ def set_headers(session_obj: requests.Session):
     session_obj.headers.update(injected_headers)
 
 
-def visit(session_obj: requests.Session, erp_url: str):
-    # session_obj = requests.Session()
+def visit(session_obj: requests.Session, erp_url: str) -> None:
     username = GLOBAL["username"]
     password = GLOBAL["password"]
 
     # First Visit
     # This will be the first web request made to ERP inorder to get
     # the session ID and store it as a cookie
-    erp_res = session_obj.get(erp_url)
-    print(f"{erp_res.status_code} - first visit status code")
+    first_visit_res = session_obj.get(erp_url)
+    print(f"{first_visit_res.status_code} - first visit status code")
 
     # Writes the first visit html data to a temporary file from
     # which the majority of the post data is extracted
     with open("temp.html", "w+") as file:
-        file.write(erp_res.text)
+        file.write(first_visit_res.text)
 
     # this will give us the new post data to be sent to the server
     data = util_helper.get_data("temp.html")
 
     # Gives Username
     data += f"&txtUserName={username}"
-    resp = session_obj.post(erp_url, data)
-    print(f"{resp.status_code} - after giving username status code")
+    uname_visit_res = session_obj.post(erp_url, data)
+    print(f"{uname_visit_res.status_code} - after giving username status code")
 
     with open("temp.html", "w+") as f:
-        f.write(resp.text)
+        f.write(uname_visit_res.text)
 
     data = util_helper.get_data("temp.html")
     os.remove("temp.html")
 
     # Gives Password
     data += f"&txtPassword={password}"
-    resp = session_obj.post(erp_url, data)
-    print(f"{resp.status_code} - after giving password status code")
+    pwd_visit_res = session_obj.post(erp_url, data)
+    print(f"{pwd_visit_res.status_code} - after giving password status code")
 
     with open("homepage.html", "w+") as f:
-        f.write(resp.content.decode("utf8"))
+        f.write(pwd_visit_res.content.decode("utf8"))
 
     print("Successfully Generated Homepage")
 
